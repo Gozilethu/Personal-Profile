91c639ef32979c3843b5a2b234d7740cffbba105
Thandazani(Mabutho) Mbutho <141316674+Gozilethu@users.noreply.github.com>
2024-04-01 11:35:21 +0200

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,9 @@
+function openImage(imageUrl = 'mabutho.jpg') {
+    window.open(imageUrl, '_blank');
+}function openImage(imageUrl = 'images/IMG-20240301-WA0001.jpg') {
+    window.open(imageUrl, '_blank');
+}function openImage(imageUrl = 'images/mabutho02.jpg') {
+    window.open(imageUrl, '_blank');
+}function openImage(imageUrl = 'images/mabutho03.jpg') {
+    window.open(imageUrl, '_blank');
+}
